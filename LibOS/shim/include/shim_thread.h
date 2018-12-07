1c0a838615935e29944513d6f083e5d151e98574
Isaku Yamahata <isaku.yamahata@gmail.com>
2018-12-06 22:00:57 -0800

    LibOS: use shim_tcb_t *, __libc_tcb_t* instead of void *

--- LibOS/shim/include/shim_thread.h
+++ LibOS/shim/include/shim_thread.h
@@ -142,8 +142,8 @@ void put_thread (struct shim_thread * thread);
 void get_simple_thread (struct shim_simple_thread * thread);
 void put_simple_thread (struct shim_simple_thread * thread);
 
-void allocate_tls (void * tcb_location, bool user, struct shim_thread * thread);
-void populate_tls (void * tcb_location, bool user);
+void allocate_tls (__libc_tcb_t * tcb_location, bool user, struct shim_thread * thread);
+void populate_tls (__libc_tcb_t * tcb_location, bool user);
 
 void debug_setprefix (shim_tcb_t * tcb);
 
