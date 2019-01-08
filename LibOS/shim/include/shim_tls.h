6330ea6488547b1f34fdc62ade495bc642dc05e3
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-01-07 22:42:52 -0800

    [LibOS] use struct debug_buf instead of void * in shim_tcb_t

--- LibOS/shim/include/shim_tls.h
+++ LibOS/shim/include/shim_tls.h
@@ -65,7 +65,7 @@ struct shim_tcb {
     struct shim_context     context;
     unsigned int            tid;
     int                     pal_errno;
-    void *                  debug_buf;
+    struct debug_buf *      debug_buf;
 
     /* This record is for testing the memory of user inputs.
      * If a segfault occurs with the range [start, end],
