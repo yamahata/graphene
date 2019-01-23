5a6fec912466046aa9530b640c1b2a966199fcaf
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-01-23 13:35:30 -0800

    [Pal/Linux-SGX] sort out struct pal_handle_thread and PAL_HANDLE
    Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

--- Pal/src/host/Linux-SGX/db_threading.c
+++ Pal/src/host/Linux-SGX/db_threading.c
@@ -67,13 +67,14 @@ PAL_IDX pal_tid_new(void)
 
 void pal_start_thread (void)
 {
-    struct pal_handle_thread *new_thread = NULL, *tmp;
+    PAL_HANDLE new_thread = NULL;
+    struct pal_handle_thread * tmp;
 
     _DkInternalLock(&thread_list_lock);
     listp_for_each_entry(tmp, &thread_list, list)
         if (!tmp->tcs) {
-            new_thread = tmp;
-            new_thread->tcs =
+            new_thread = container_of(tmp, struct pal_handle, thread);
+            new_thread->thread.tcs =
                 enclave_base + GET_ENCLAVE_TLS(tcs_offset);
             break;
         }
@@ -83,11 +84,11 @@ void pal_start_thread (void)
         return;
 
     struct thread_param * thread_param =
-            (struct thread_param *) new_thread->param;
+            (struct thread_param *) new_thread->thread.param;
     int (*callback) (void *) = thread_param->callback;
     const void * param = thread_param->param;
     free(thread_param);
-    new_thread->param = NULL;
+    new_thread->thread.param = NULL;
     SET_ENCLAVE_TLS(self,
                     GET_ENCLAVE_TLS(tls_offset) + enclave_base);
     SET_ENCLAVE_TLS(thread, new_thread);
