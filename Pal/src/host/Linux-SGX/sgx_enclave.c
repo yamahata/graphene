c7c1c3911900d301763107e20d90fde6e1be02ad
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-01-29 18:28:47 -0800

    [Pal/Linux-SGX] sgx_enclave.c typo in debug message
    Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

--- Pal/src/host/Linux-SGX/sgx_enclave.c
+++ Pal/src/host/Linux-SGX/sgx_enclave.c
@@ -293,7 +293,7 @@ static int sgx_ocall_futex(void * pms)
         ts->tv_sec = ms->ms_timeout / 1000000;
         ts->tv_nsec = (ms->ms_timeout - ts->tv_sec * 1000000) * 1000;
     }
-    SGX_DBG(DBG_E, "futext %p op %d val %d timeout %ld\n",
+    SGX_DBG(DBG_E, "futex %p op %d val %d timeout %ld\n",
             ms->ms_futex, ms->ms_op, ms->ms_val, ms->ms_timeout);
     ret = INLINE_SYSCALL(futex, 6, ms->ms_futex, ms->ms_op, ms->ms_val,
                          ts, NULL, 0);
